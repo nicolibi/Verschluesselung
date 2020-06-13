f5aa45bbe2a447771c8bf397b117e2f9065a7fb6
Nicole Bieger <nicoleregister@email.de>
2020-06-13 03:14:22 +0200

    Akualisiert aufgrund der Musterlösung, speziell bei  der Ermittlung der neuen Position

--- Verschluesserlung/Program.cs
+++ Verschluesserlung/Program.cs
@@ -8,10 +8,10 @@ namespace Verschluesserlung
 {
     class Program
     {
-        public static string OriginalString = "";
+        public static string ZuVerschluesselnderText = "";
         private static string grossbuchstabenString;
         private static char[] charArray;
-        private const int charShift = -4;
+        private const int charShift = 13;
         private static int shiftedPosition = 0;
         private static int unicodeOfChar;
         private static int newPosition = 0;
@@ -22,27 +22,59 @@ namespace Verschluesserlung
         static void Main(string[] args)
         {
             //int position = 75;
-            OriginalString = "Asvph";
 
-            Console.WriteLine("neuer String: {0}", Verschluesseln(OriginalString));
+            #region Benutzereingabe abfragen
+            //Abfrage des zu verschlüsselnden Textes
+            ZuVerschluesselnderText = BenutzerNachEingabeFragen();
+            #endregion
+
+            #region Verschlüsseln und Ausgabe verschlüsselter String
+            EncryptedString = VerschluesselnMitRot13(ZuVerschluesselnderText);
+
+            Console.WriteLine("neuer String: {0}", EncryptedString);
+            #endregion
+
+            #region Abschluss
+            Console.WriteLine("");
+            Console.WriteLine("Zum Beenden bitte ENTER drücken");
             Console.ReadLine();
+            #endregion
 
         }
 
-        public static string Verschluesseln(string originalstring)
+        public static string BenutzerNachEingabeFragen()
+        {
+            Console.WriteLine("Bitte gib zu den verschlüsselnden Text ein");
+            return Console.ReadLine();
+        }
+
+        private static string NormalisiereString(string unverschluesselterString)
+        {
+            string normalisierterString = unverschluesselterString;
+            normalisierterString = normalisierterString.Replace("Ä", "AE");
+            normalisierterString = normalisierterString.Replace("Ö", "OE");
+            normalisierterString = normalisierterString.Replace("Ü", "UE");
+            normalisierterString = normalisierterString.Replace("ß", "SS");
+
+            return normalisierterString;
+        }
+
+
+
+        public static string VerschluesselnMitRot13(string originalstring)
         {
             grossbuchstabenString = originalstring.ToUpper();
             charArray = grossbuchstabenString.ToCharArray();
             foreach (char ch in charArray)
             {
                 unicodeOfChar = Convert.ToUInt16(ch);
-                if (unicodeOfChar >= 65 && unicodeOfChar <= 90)
+                if (char.IsLetter(ch))
                 {
-
                     shiftedPosition = (unicodeOfChar + charShift);
-                    if (shiftedPosition > 90)
+                    int ueberhang = shiftedPosition - 90;
+                    if (ueberhang > 0)
                     {
-                        newPosition = ((shiftedPosition - 65) % 26) + 65;
+                        newPosition = (ueberhang - 1) + 65;
                     }
                     else
                     {
@@ -60,7 +92,8 @@ namespace Verschluesserlung
 
             return EncryptedString;
         }
-
-
     }
+
+
 }
+
